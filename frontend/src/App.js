8169ed0c11ea00e8b014374b3c10004b48c4eb01
Sethu Narayan Senthil <github@sethusenthil.com>
2022-02-14 18:45:42 -0500

    Do basic UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import NavBar from './Components/NavBar';
+import ConcertTile from './Components/ConcertTile';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import Grid from '@mui/material/Grid';
+
+
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#6002ee',
+      },
+      secondary: {
+        main: '#90ee02',
+      },
+    },
+  });
   return (
+    <ThemeProvider theme={darkTheme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
+     <div style={{display: 'flex', flexDirection: 'row', margin: 20}}>
+      <TextField id="filled-basic" label="Search" variant="filled" fullWidth/>
+      &nbsp; &nbsp;
+      <Button variant="outlined" startIcon={<FilterListIcon />}>
+  Filter
+</Button>
+
+     </div>
+     <Grid container spacing={2}>
+  <Grid item xs={3}>
+  <ConcertTile/>
+  </Grid>
+  <Grid item xs={3}>
+  <ConcertTile/>
+  </Grid>
+  <Grid item xs={3}>
+  <ConcertTile/>
+  </Grid>
+  <Grid item xs={3}>
+  <ConcertTile/>
+  </Grid>
+  <Grid item xs={3}>
+  <ConcertTile/>
+  </Grid>
+</Grid>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/Components/ConcertTile.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import AttachMoney from '@mui/icons-material/AttachMoney';
+import BuyNow from '@mui/icons-material/ShoppingCartCheckout';
+import Place from '@mui/icons-material/Place';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function ConcertTile() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Artist Name"
+        subheader="Date"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Thumnail Image"
+      />
+      <CardActions disableSpacing>
+        <IconButton aria-label="price">
+          <AttachMoney /> <Typography variant="caption">000</Typography>
+        </IconButton>
+
+        <IconButton aria-label="location">
+
+          <Place /> <Typography variant="caption">NJ</Typography>
+        </IconButton>
+
+        <Button variant="outlined" startIcon={<BuyNow />}>
+        Buy
+      </Button>
+      </CardActions>
+    </Card>
+  );
+}
