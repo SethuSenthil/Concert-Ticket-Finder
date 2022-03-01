82a9cdeb1b6441b46166250f25b3472b57fef80b
Sethu Narayan Senthil <github@sethusenthil.com>
2022-03-01 01:07:55 -0500

    api

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 4566
+const axios = require("axios").default;
+require('dotenv').config()
+
+const AUTH_STRING = `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&`
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/getHomeEvents', (req, res) => {
+  var options = {
+    method: 'GET',
+    url: `https://api.seatgeek.com/2/events?${AUTH_STRING}postal_code=${'19001'}&q=rap`,
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+    res.send(response.data)
+  }).catch(function (error) {
+    console.error(error);
+  });
 })
 
 app.listen(port, () => {
