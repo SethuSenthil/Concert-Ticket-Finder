94e8769184efb2254e415d58d48e92f32a28d70a
Sethu Narayan Senthil <github@sethusenthil.com>
2022-03-05 20:43:24 -0500

    change from ZIP to GeoIP

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const port = process.env.PORT || 4566
 const axios = require("axios").default;
 require('dotenv').config()
 
+app.set('trust proxy', true) //to get use up
+
 const AUTH_STRING = `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&`
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('your ip is: ' + req.ip)
 })
 
 app.get('/getHomeEvents', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   var options = {
     method: 'GET',
-    url: `https://api.seatgeek.com/2/events?${AUTH_STRING}postal_code=${'19001'}&q=rap`,
+    url: `https://api.seatgeek.com/2/events?${AUTH_STRING}geoip=${req.ip}&q=rap&range=12mi`,
   };
 
   axios.request(options).then(function (response) {
